c44d4ee7a29d195f2c1c591e568fc891d077bd27
Taras Doloshytskyi <doloshytskyi@afreshed.at>
2022-05-25 12:50:26 +0300

    udemy_lesson_39

--- src/components/ExpenceItem.jsx
+++ src/components/ExpenceItem.jsx
@@ -1,18 +1,12 @@
 import react from 'react';
 import './ExpenceItem.css';
+import ExpenceDate from './ExpenceDate.jsx';
 function ExpenceItem(props) {
-    const month = props.date.toLocaleString('en-US',{month:'long'})
-    const date = props.date.toLocaleString('en-US',{day:'2-digit'})
-    const year = props.date.getFullYear()
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{date}</div>
-                <div>{year}</div>
-            </div>
-            <div>
-                <h2 className="expense-item__description">{props.title}</h2>
+            <ExpenceDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
