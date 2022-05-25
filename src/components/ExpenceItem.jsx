be9883c866a14aadd9bb602b614c356a2fdcb2dc
Taras Doloshytskyi <doloshytskyi@afreshed.at>
2022-05-25 15:45:56 +0300

    udemy_lesson_49_useState

--- src/components/ExpenceItem.jsx
+++ src/components/ExpenceItem.jsx
@@ -1,14 +1,22 @@
 import './ExpenceItem.css';
+import { useState } from 'react';
 import ExpenceDate from './ExpenceDate.jsx';
 import Card from './Card';
 const ExpenceItem = props => {
+    const [title,setTitle] = useState(props.title)
+
+    
+    const clickHandler = () => {
+        setTitle('changed')
+    };
     return (
         <Card className="expense-item">
             <ExpenceDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 };
