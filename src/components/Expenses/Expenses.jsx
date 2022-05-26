1f17118edcdda0902c58e760c7252e8974908394
Taras Doloshytskyi <doloshytskyi@afreshed.at>
2022-05-26 13:44:43 +0300

    udemy_part_3_3_filteredExpences

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,24 +4,30 @@ import ExpensesFilter from './ExpensesFilter.jsx';
 import React, { useState } from 'react';
 import './Expenses.css';
 const Expenses = props => {
-    const [year, setYear] = useState('');
+    const [year, setYear] = useState(new Date().getFullYear());
     const onChangeYear = value => {
+        debugger
         setYear(value);
     };
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter year={year} setYear={onChangeYear} />
-                {props.items.map(element => {
-                    return (
-                        <ExpenceItem
-                            title={element.title}
-                            amount={element.amount}
-                            date={element.date}
-                            key={element.id}
-                        />
-                    );
-                })}
+                {props.items
+                    .filter(element => {
+                        return element.date.getFullYear() == year;
+                    })
+                    .map(element => {
+                        return (
+                            <ExpenceItem
+                                title={element.title}
+                                amount={element.amount}
+                                date={element.date}
+                                key={element.id}
+                            />
+                        );
+                    })}
             </Card>
         </div>
     );
